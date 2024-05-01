8500047201163ced7dcdf507ee81181dcbfa1ab2
Gleb <143495074+GlebKruchkov@users.noreply.github.com>
2024-05-01 21:31:14 +0300

    Delete long_path.py

--- long_path.py
+++
@@ -1,5 +0,0 @@
-import networkx as nx
-with open("dependencies.dot") as f:
-    graph = nx.drawing.nx_pydot.read_dot(f)
-    paths = nx.all_simple_paths(graph, source=list(graph.nodes)[0], target=list(graph.nodes)[-1])
-    print(max(paths, key=len))
